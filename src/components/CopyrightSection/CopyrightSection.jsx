8e812b9a324c40286fc905658373f9f9676b4e34
Ibrohim Tuxtayev <itoxtayev07@gmail.com>
2026-05-06 18:59:02 +0500

    feat: create CopyrightSection component

--- src/components/CopyrightSection/CopyrightSection.jsx
+++ src/components/CopyrightSection/CopyrightSection.jsx
@@ -1,6 +1,6 @@
 
 export function CopyrightSection() {
-  return <section className="copyright-sect w-full max-w-full border-t-[1px] border-solid border-[#DBDFEA]">
-    <p className="login-copyright-text font-normal text-[14px] leading-[165%] tracking-normal text-[#8091A7]">© 2024 Developed by Open Web</p>
+  return <section className="copyright-sect w-full max-w-full mt-auto p-[20px_24px] flex items-center justify-center border-t-[1px] border-solid border-[#DBDFEA] bg-[#FFF]">
+    <p className="login-copyright-text font-normal text-[14px] leading-[165%] tracking-normal text-[#B79F7C]">© 2024 Developed by Open Web</p>
   </section>
 }
