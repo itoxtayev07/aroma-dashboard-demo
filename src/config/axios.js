a8bda6fc6d80c37b0ea8459447573d48be64e457
Ibrohim Tuxtayev <itoxtayev07@gmail.com>
2026-05-06 18:32:09 +0500

    feat: install shadcn's pagination and fix a couple of things

---
+++ src/config/axios.js
@@ -0,0 +1,21 @@
+import axios from "axios"
+
+export const api = axios.create({
+    baseURL: "https://api.aroma.localhost.uz",
+    timeout: 30000,
+})
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token")
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+})
+
+export const login = async (username, password) => {
+    const response = await api.post("/admin/login", { username, password })
+    console.log("Login response:", response.data)
+    console.log("Token:", localStorage.getItem("token"))
+    return response.data
+}
