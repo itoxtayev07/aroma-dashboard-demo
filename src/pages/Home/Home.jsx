59e01caa9fbbcf55be78fc74c44749e0f7cf42fe
Ibrohim Tuxtayev <itoxtayev07@gmail.com>
2026-05-10 20:27:09 +0500

    feat create unfinished Home page and add images

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+export function Home() {
+    return (
+        <section className=' w-full max-w-full p-[32px] flex-1'>
+            <h1 className="text-[#364A63] text-[28px] font-bold leading-[110%] tracking-[-0.84px]">Home Page</h1>
+        </section>
+    )
+}
